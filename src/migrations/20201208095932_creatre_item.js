3d8b6da6ed4bedf71602f4c9496c3f3c73f057ec
竹園隆聖 <ryusei.takezono@gmail.com>
2020-12-09 12:24:07 +0900

    fix

--- src/migrations/20201208095932_creatre_item.js
+++ src/migrations/20201208095932_creatre_item.js
@@ -2,11 +2,11 @@ exports.up = function(knex) {
   return knex.schema.createTable("items", (table) => {
     table.increments().primary();
     table.string("item_name", 20).notNull();
-    table.integer("item_price", 6).notNull();
+    table.integer("item_price", 10).notNull();
     table.integer("item_month", 2).notNull();
     table.integer("item_day", 2) /*.notNull()*/;
     table
-      .integer("tag_id", 4)
+      .integer("tag_id", 3)
       .unsigned()
       .references("id")
       .inTable("tags");
