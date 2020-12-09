d15997d7ce5a74ff57482b03cfea0b614805c124
竹園隆聖 <ryusei.takezono@gmail.com>
2020-12-09 12:09:07 +0900

    test

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 module.exports = {
   client: "pg",
   connection:
-    process.env.DB_URL || `postgres://${process.env.USER}@127.0.0.1:5432/mymvp`,
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.USER}@127.0.0.1:5432/mymvp`,
   migrations: {
     directory: "./src/migrations",
   },
